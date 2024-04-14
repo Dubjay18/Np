2aab2978d5a9562285daea17dc5b770933f494cc
Dubjay18 <jejeniyi7@gmail.com>
2024-04-14 17:31:22 +0100

    feat:sort button in fitler comp

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,89 +1,91 @@
 import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { DarkTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
+import {DarkTheme, ThemeProvider} from '@react-navigation/native';
+import {useFonts} from 'expo-font';
+import {Stack} from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
+import {useEffect} from 'react';
 
-import { useColorScheme } from '@/components/useColorScheme';
+import {useColorScheme} from '@/components/useColorScheme';
 import {DefaultTheme, PaperProvider, useTheme} from "react-native-paper";
 import {BottomSheetModalProvider} from "@gorhom/bottom-sheet";
 import {GestureHandlerRootView} from "react-native-gesture-handler";
 
 export {
-  // Catch any errors thrown by the Layout component.
-  ErrorBoundary,
+    // Catch any errors thrown by the Layout component.
+    ErrorBoundary,
 } from 'expo-router';
 
 export const unstable_settings = {
-  // Ensure that reloading on `/modal` keeps a back button present.
-  initialRouteName: '(tabs)',
+    // Ensure that reloading on `/modal` keeps a back button present.
+    initialRouteName: '(tabs)',
 };
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  const [loaded, error] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-    ...FontAwesome.font,
-  });
+    const [loaded, error] = useFonts({
+        SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+        ...FontAwesome.font,
+    });
 
-  // Expo Router uses Error Boundaries to catch errors in the navigation tree.
-  useEffect(() => {
-    if (error) throw error;
-  }, [error]);
+    // Expo Router uses Error Boundaries to catch errors in the navigation tree.
+    useEffect(() => {
+        if (error) throw error;
+    }, [error]);
 
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
+    useEffect(() => {
+        if (loaded) {
+            SplashScreen.hideAsync();
+        }
+    }, [loaded]);
+
+    if (!loaded) {
+        return null;
     }
-  }, [loaded]);
 
-  if (!loaded) {
-    return null;
-  }
-
-  return <RootLayoutNav />;
+    return <RootLayoutNav/>;
 }
 const theme = {
-  ...DefaultTheme,
-  // Specify custom property
-  myOwnProperty: true,
-  // Specify custom property in nested object
-  colors: {
-    ...DefaultTheme.colors,
-    myOwnColor: '#BADA55',
-    primary: 'rgb(10, 132, 255)',
-    background: 'rgb(1, 1, 1)',
-    card: 'rgb(18, 18, 18)',
-    text: 'rgb(255,255,255)',
-    border: 'rgb(39, 39, 41)',
-    notification: 'rgb(255, 69, 58)',
-    onSurfaceVariant: 'rgb(255,255,255)'
-  },
+    ...DefaultTheme,
+    // Specify custom property
+    myOwnProperty: true,
+    // Specify custom property in nested object
+    colors: {
+        ...DefaultTheme.colors,
+        myOwnColor: '#BADA55',
+        primary: 'rgb(10, 132, 255)',
+        background: 'rgb(1, 1, 1)',
+        card: 'rgb(18, 18, 18)',
+        text: 'rgb(255,255,255)',
+        border: 'rgb(10, 132, 255)',
+        notification: 'rgb(255, 69, 58)',
+        onSurfaceVariant: 'rgb(255,255,255)',
+        outline: 'rgb(10, 132, 255)',
+    },
 };
 export type AppTheme = typeof theme;
 
 export const useAppTheme = () => useTheme<AppTheme>();
+
 function RootLayoutNav() {
-  const colorScheme = useColorScheme();
+    const colorScheme = useColorScheme();
 
 
-  return (
-      <GestureHandlerRootView style={{
+    return (
+        <GestureHandlerRootView style={{
             flex: 1,
 
-      }}>
-      <BottomSheetModalProvider >
-      <PaperProvider theme={theme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="search" options={{ presentation: 'modal' }} />
-      </Stack>
-      </PaperProvider>
-      </BottomSheetModalProvider>
-</GestureHandlerRootView>
+        }}>
+            <BottomSheetModalProvider>
+                <PaperProvider theme={theme}>
+                    <Stack>
+                        <Stack.Screen name="(tabs)" options={{headerShown: false}}/>
+                        <Stack.Screen name="search" options={{presentation: 'modal'}}/>
+                    </Stack>
+                </PaperProvider>
+            </BottomSheetModalProvider>
+        </GestureHandlerRootView>
 
-  );
+    );
 }
--- components/FilterBottomSheet.tsx
+++ components/FilterBottomSheet.tsx
@@ -1,13 +1,31 @@
-import React, { useCallback, useMemo, useRef } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, {useCallback, useMemo, useRef} from 'react';
+import {View, Text, StyleSheet} from 'react-native';
 import {
     BottomSheetModal,
     BottomSheetView,
     BottomSheetModalProvider,
 } from '@gorhom/bottom-sheet';
 import {Button} from "react-native-paper";
+import {useAppTheme} from "@/app/_layout";
+import Spacer from "@/components/Spacer";
 
 const FilterBottomSheet = () => {
+    const {colors} = useAppTheme();
+    const sortArray = [
+        "Recommended",
+        "Latest",
+        "Most Viewed",
+        "Following"
+    ]
+    const [sort, setSort] = React.useState<string[]>([])
+
+    const handleSort = (item: string) => {
+        if (sort.includes(item)) {
+            setSort(sort.filter(i => i !== item))
+            return
+        }
+        setSort([...sort, item])
+    }
     // ref
     const bottomSheetModalRef = useRef<BottomSheetModal>(null);
 
@@ -26,29 +44,97 @@ const FilterBottomSheet = () => {
     return (
 
 
-            <View style={styles.container}>
-                <Button
-                    onPress={handlePresentModalPress}
-                   icon={"filter"}
-                >Filter</Button>
-                <BottomSheetModal
-                    ref={bottomSheetModalRef}
-                    index={1}
-                    snapPoints={snapPoints}
-                    onChange={handleSheetChanges}
-                >
-                    <BottomSheetView style={styles.contentContainer}>
-                        <Text>Awesome 🎉</Text>
-                    </BottomSheetView>
-                </BottomSheetModal>
-            </View>
+        <View style={styles.container}>
+            <Button
+                onPress={handlePresentModalPress}
+                icon={"filter"}
+            >Filter</Button>
+            <BottomSheetModal
+                ref={bottomSheetModalRef}
+                index={1}
+                snapPoints={snapPoints}
+                onChange={handleSheetChanges}
+            >
+                <BottomSheetView style={styles.contentContainer}>
+                    <View style={{
+                        flexDirection: "row",
+                        justifyContent: "space-between",
+                        width: "90%",
+                        marginTop: 30,
+                        alignItems: "center"
+                    }}>
+                        <Text style={{
+                            color: "white",
+                            fontWeight: "bold",
+                            fontSize: 24
+                        }}>Filter</Text>
+                        <Button
+                            onPress={() => {
+                                console.log("Reset")
+                            }}
+                            buttonColor={colors?.primary}
+                            mode={"outlined"}
+                            style={{
+                                borderColor: "white",
+                                borderWidth: 1,
+                                borderRadius: 50,
+                                borderStyle: 'solid',
+
+                            }}
+
+                            icon={"delete"}
+                            textColor={"white"}
+                        >Reset</Button>
+                    </View>
+
+                    <View style={{
+                        width: "90%",
+                    }}>
+                        <Spacer size={20}/>
+                        <Text style={{
+                            color: "white",
+                            fontSize: 20
+
+                        }}>Sort By</Text>
+                        <Spacer size={20}/>
+                        <View style={{
+                            flexDirection: "row",
+                            width: "100%",
+                            flexWrap: "wrap",
+                            gap: 10
+                        }}>
+
+                            {
+                                sortArray.map((item, index) => {
+                                    return (
+                                        <View key={index}>
+
+                                            <Button
+                                                onPress={() => {
+                                                    handleSort(item)
+
+                                                }}
+                                                mode={"outlined"}
+                                                textColor={"white"}
+                                                contentStyle={{flexDirection: 'row-reverse'}}
+                                                buttonColor={sort.includes(item) ? colors?.primary : "transparent"}
+                                                icon={sort.includes(item) ? "cancel" : ""}
+                                            >{item}</Button>
+                                        </View>
+                                    )
+                                })
+                            }
+                        </View>
+                    </View>
+                </BottomSheetView>
+            </BottomSheetModal>
+        </View>
 
     );
 };
 
 const styles = StyleSheet.create({
-    container: {
-    },
+    container: {},
     contentContainer: {
         flex: 1,
         alignItems: 'center',
